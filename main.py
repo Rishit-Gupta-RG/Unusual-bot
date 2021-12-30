6a1847df52343a50cf03288fe1a4e2f49cd5f818
Rishit Gupta <gamingsandroking@gmail.com>
2021-12-30 15:34:22 +0530

    chore: last test

--- main.py
+++ main.py
@@ -148,7 +148,8 @@ async def say(ctx, *, message):
 #WELCOME
 @bot.event
 async def on_member_join(member):
-    await bot.get_channel(908296505876688958).send(f"Hello {member.mention} :wave:\nWelcome to {member.guild.name} ❤\n\n- <#809297410979397663> - read the server rules\n- <#850694848323256360> - pick up roles\n- <#900106358823739442> - verify yourself\n- <#908002039907373108> - check out all the channels")
+    welcomechannel = bot.get_channel(9908296505876688958)
+    await welcomechannel.send(f"Hello {member.mention} :wave:\nWelcome to {member.guild.name} ❤\n\n- <#809297410979397663> - read the server rules\n- <#850694848323256360> - pick up roles\n- <#900106358823739442> - verify yourself\n- <#908002039907373108> - check out all the channels")
 
 #HELP SUB-COMMANDS
 @help.command()
