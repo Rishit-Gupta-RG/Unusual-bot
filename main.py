13259d3632dcc6bc9e26c1a6d762cff73da0eeff
Rishit Gupta <gamingsandroking@gmail.com>
2022-01-01 00:16:37 +0530

    chore: ahh

--- main.py
+++ main.py
@@ -142,6 +142,7 @@ async def dog(ctx):
       dogjson = await request.json()
         
     embed = discord.Embed(title="Doggo!", color=ctx.author.color())
+    embed.set_author(name= "Unusual Friends#3075", icon_url="https://images-ext-1.discordapp.net/external/0k2KhTElwr7ki38RUYVAAsCKjwPZ5wP32DFQpt37k2M/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/835206086570803211/d7c4dbb2521c985489e973aea0cb5a2e.png?width=473&height=473")
     embed.set_image(url=dogjson['link'])
     await ctx.send(embed=embed)
 
