a01c4b11c0bb1f192ef5daa4b59b131454e40a14
Rishit Gupta <gamingsandroking@gmail.com>
2022-01-08 20:28:28 +0530

    updateing to disnake: first commit

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
+from logging import fatal
 import disnake
+from disnake import channel
+from disnake import embeds
+from disnake.embeds import Embed
+import asyncio
 from disnake.ext import commands
+import aiohttp 
+import datetime
+import time
+import re
+from disnake import Webhook, RequestsWebhookAdapter, File
+from disnake.ext.commands import cooldown, BucketType
+import sys
 import os
 from dotenv import load_dotenv
-load_dotenv()
+import youtube_dl
+import random
+from disnake import Member
+from disnake.ext.commands import has_permissions, MissingPermissions
+from urllib import parse, request
+from disnake.ext.commands.bot import Bot
+from disnake.ext.commands.converter import EmojiConverter
+from disnake.ext.commands.core import command
+from disnake.utils import get
+from disnake import TextChannel
+from youtube_dl import YoutubeDL
+import json
+import contextlib
+import io
+import datetime, time
+import psutil
 
-bot = commands.Bot(command_prefix=">")
+
+
+bot = commands.Bot(command_prefix="!")
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send("Pong!")
+    before = time.monotonic()
+    message = await ctx.send("Pong!")
+    ping = (time.monotonic() - before) * 1000
+    await message.edit(content=f"Pong!  `{int(ping)}ms`")
 
 @bot.event
 async def on_ready():
