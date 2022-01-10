583d4d7e19751c448eaca787da72ff48821e5089
Rishit Gupta <gamingsandroking@gmail.com>
2022-01-10 20:41:28 +0530

    0.30

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ async def timeout(ctx, member: disnake.Member,time, *, reason=None) -> None:
 @bot.command()
 @disnake.ext.commands.has_permissions(manage_nicknames=True)
 async def rto(ctx, member: disnake.Member, reason=None) -> None:
-    await member.timeout(reason=reason)
+    await member.timeout(duration=None)
     await ctx.send(f"Timeout for {member.mention} has been removed by {ctx.author.mention}.\n**Reason -** {reason}")
 
 
