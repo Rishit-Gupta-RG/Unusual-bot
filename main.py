ffc1ebba7f75512c19c2709408e7ff977da62a08
Rishit Gupta <gamingsandroking@gmail.com>
2022-01-20 18:38:00 +0530

    0.45

--- main.py
+++ main.py
@@ -139,51 +139,6 @@ async def google(ctx: commands.Context, *, query: str):
     view.add_item(button(label="Click here", url=url))
     await ctx.send(f"Google Result for: `{query}`", view=view)
 
-@bot.command(pass_context=True, hidden=True, name='run')
-async def run(self, ctx, *, body: str):
-
-    env = {
-        'bot': self.bot,
-        'ctx': ctx,
-        'channel': ctx.channel,
-        'author': ctx.author,
-        'guild': ctx.guild,
-        'message': ctx.message,
-        '_': self._last_result
-    }
-
-    env.update(globals())
-
-    body = self.cleanup_code(body)
-    stdout = io.StringIO()
-
-    to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
-
-    try:
-        exec(to_compile, env)
-    except Exception as e:
-        return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
-
-    func = env['func']
-    try:
-        with redirect_stdout(stdout):
-            ret = await func()
-    except Exception as e:
-        value = stdout.getvalue()
-        await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-    else:
-        value = stdout.getvalue()
-        try:
-            await ctx.message.add_reaction('\u2705')
-        except:
-            pass
-
-        if ret is None:
-            if value:
-                await ctx.send(f'```py\n{value}\n```')
-        else:
-            self._last_result = ret
-            await ctx.send(f'```py\n{value}{ret}\n```')
 
 @bot.listen('on_command_error')
 async def error_handler(ctx, error):
@@ -194,4 +149,5 @@ async def on_ready():
     await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.streaming, name="The Monke Game", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
     print('Bot is ready')
 
+bot.load_extension('jishaku')
 bot.run(os.getenv("TOKEN"))
