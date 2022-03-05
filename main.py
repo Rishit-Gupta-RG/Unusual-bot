0cf9c5b103104ff941c27610bf6bd9634241d6e0
Rishit Gupta <gamingsandroking@gmail.com>
2022-03-05 23:50:58 +0530

    k

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ import inspect
 from typing import Union, Optional
 from contextlib import redirect_stdout
 from disnake.enums import TextInputStyle
+from inspect import getsource
 
 intents = disnake.Intents.default()
 intents.presences = True
@@ -65,6 +66,7 @@ async def ping(ctx):
     ping = (time.monotonic() - before) * 1000
     await message.edit(content=f"Pong!  `{int(ping)}ms`")
 
+"""
 @bot.command(name="evaluate", aliases=["e", "eval"], description="Runs a python script.")
 async def evaluate(ctx, *, code):
     str_obj = io.StringIO()
@@ -74,6 +76,7 @@ async def evaluate(ctx, *, code):
     except Exception as e:
         return await ctx.send(f"```{e.__class__.__name__}: {e}```")
     await ctx.send(f'```{str_obj.getvalue()}```')
+"""
 
 @bot.command(name="timeout", description="Timeout a user.", aliases=['mute'])
 @disnake.ext.commands.has_permissions(manage_nicknames=True)
@@ -409,6 +412,58 @@ monke = str"monkelife"
 print(monke)```""", color=ctx.author.color)
     embed.set_footer(text="Note: These are backticks not quotes.")
     await ctx.send(embed=embed)
+
+class EvalCommand:
+    def __init__(self):
+        pass
+    
+    def resolve_variable(self, variable):
+        if hasattr(variable, "__iter__"):
+            var_length = len(list(variable))
+            if (var_length > 100) and (not isinstance(variable, str)):
+                return f"<a {type(variable).__name__} iterable with more than 100 values ({var_length})>"
+            elif (not var_length):
+                return f"<an empty {type(variable).__name__} iterable>"
+        
+        if (not variable) and (not isinstance(variable, bool)):
+            return f"<an empty {type(variable).__name__} object>"
+        return (variable if (len(f"{variable}") <= 1000) else f"<a long {type(variable).__name__} object with the length of {len(f'{variable}'):,}>")
+    
+    def prepare(self, string):
+        arr = string.strip("```").replace("py\n", "").replace("python\n", "").split("\n")
+        if not arr[::-1][0].replace(" ", "").startswith("return"):
+            arr[len(arr) - 1] = "return " + arr[::-1][0]
+        return "".join(f"\n\t{i}" for i in arr)
+    
+    @commands.command(pass_context=True, aliases=['eval', 'exec', 'evaluate'])
+    @commands.is_owner()
+    async def _eval(self, ctx, *, code: str):
+        silent = ("-s" in code)
+        
+        code = self.prepare(code.replace("-s", ""))
+        args = {
+            "disnake": disnake,
+            "sauce": getsource,
+            "sys": sys,
+            "os": os,
+            "imp": __import__,
+            "this": self,
+            "ctx": ctx
+        }
+        
+        try:
+            exec(f"async def func():{code}", args)
+            a = time()
+            response = await eval("func()", args)
+            if silent or (response is None) or isinstance(response, disnake.Message):
+                del args, code
+                return
+            
+            await ctx.send(f"```py\n{self.resolve_variable(response)}````{type(response).__name__} | {(time() - a) / 1000} ms`")
+        except Exception as e:
+            await ctx.send(f"Error occurred:```\n{type(e).__name__}: {str(e)}```")
+        
+        del args, code, silent
 
 @bot.listen('on_command_error')
 async def error_handler(ctx, error):
