3df406ef4844e323e94d6f8aca956ebdd50c8bed
Rishit Gupta <gamingsandroking@gmail.com>
2022-09-11 21:36:07 +0530

    ggg

--- main.py
+++ main.py
@@ -67,6 +67,8 @@ if __name__ == '__main__':
 
 bot.load_extension('jishaku')
 
+jot = bot.get_emoji(958469316393705523)
+
 #PREFIX BASED
 @bot.command(name="ping", description="Shows bot latency.")
 async def ping(ctx):
@@ -87,10 +89,13 @@ async def evaluate(ctx, *, code):
 #------------------------------------------------------------------------------------------------------
 
 #SLASH COMMANDS
-Party = commands.option_enum(['Watch Together', 'Chess'])
+Party = ['Watch Together', 'chess']
+
+async def autocomplete_langs(inter, string: str) -> List[str]:
+    return [lang for lang in Party if string.lower() in lang.lower()]
 
 @bot.slash_command(name="activity", description="[BETA COMMAND] Starts voice channel activity.")
-async def activity(inter: disnake.CommandInteraction,channel: disnake.VoiceChannel ,party: Party):
+async def activity(inter: disnake.ApplicationCommandInteraction,channel: disnake.VoiceChannel ,party:  str = commands.Param(autocomplete=autocomplete_langs)):
     """
     Parameters
     ----------
@@ -99,9 +104,9 @@ async def activity(inter: disnake.CommandInteraction,channel: disnake.VoiceChann
     party: Select a party type from the above list only
     """
     if party == "Watch Together":
-        invite = await channel.create_invite(target_type=disnake.InviteTarget.embedded_application, target_application=disnake.PartyType.watch_together)
+        invite = await channel.create_invite(target_type=disnake.InviteTarget.embedded_application, target_application=disnake.PartyType.chess)
         await inter.response.send_message(f"[Click to open Watch Together in {channel}]({invite})")
-    elif party == "Chess":
+    elif Party == "chess":
         invite = await channel.create_invite(target_type=disnake.InviteTarget.embedded_application, target_application=disnake.PartyType.chess)
         await inter.response.send_message(f"[Click to open Chess in {channel}]({invite})")
     else:
@@ -119,7 +124,7 @@ async def animal(inter):
     pass
 
 @animal.sub_command(description="Sends a picture of selected animal.")
-async def image(inter: disnake.CommandInteraction, animal: str = commands.Param(autocomplete=autocomp_animals)):
+async def image(inter: disnake.ApplicationCommandInteraction, animal: str = commands.Param(autocomplete=autocomp_animals)):
     """
     Sends a picture of selected animal.
     
@@ -134,19 +139,21 @@ async def image(inter: disnake.CommandInteraction, animal: str = commands.Param(
             whalejson = await request.json()
         embed = disnake.Embed(title=f"{animal}!", color=inter.author.color)
         embed.set_image(url=whalejson['link'])
-        await inter.response.send_message(embed=embed)
+        view = Delete(inter)
+        await inter.response.send_message(embed=embed, view=view)
     elif animal == "Red Panda":
         async with aiohttp.ClientSession() as session:
             request = await session.get(f'https://some-random-api.ml/img/red_panda')
             whalejson = await request.json()
         embed = disnake.Embed(title=f"{animal}!", color=inter.author.color)
         embed.set_image(url=whalejson['link'])
-        await inter.response.send_message(embed=embed)
+        view = Delete(inter)
+        await inter.response.send_message(embed=embed, view=view)
     else:
         await inter.response.send_message("Select animal from autocomplete only!", ephemeral=True)
 
 @animal.sub_command(description="Sends a random fact of selected animal.")
-async def fact(inter: disnake.CommandInteraction, animal: str = commands.Param(autocomplete=autocomp_animalfact)):
+async def fact(inter: disnake.ApplicationCommandInteraction, animal: str = commands.Param(autocomplete=autocomp_animalfact)):
     """
     Sends a random fact of selected animal.
     
@@ -160,35 +167,39 @@ async def fact(inter: disnake.CommandInteraction, animal: str = commands.Param(a
             request = await session.get(f'https://some-random-api.ml/facts/{k}')
             whalejson = await request.json()
         embed = disnake.Embed(title=f"{animal} Fact!",description=whalejson['fact'],color=inter.author.color)
-        await inter.response.send_message(embed=embed)
+        view = Delete(inter)
+        await inter.response.send_message(embed=embed, view=view)
     elif animal == "Red Panda":
         async with aiohttp.ClientSession() as session:
             request = await session.get(f'https://some-random-api.ml/facts/red_panda')
             whalejson = await request.json()
         embed = disnake.Embed(title=f"{animal} Fact!",description=whalejson['fact'],color=inter.author.color)
-        await inter.response.send_message(embed=embed)
+        view = Delete(inter)
+        await inter.response.send_message(embed=embed, view=view)
     else:
         await inter.response.send_message("Select animal from autocomplete only!", ephemeral=True)
 
-@bot.command()
+@bot.slash_command(name="spam", description="")
 @commands.cooldown(1,35,commands.BucketType.guild)
-async def spam(ctx, Amount : int, *, Message=None):
-    if ctx.channel.id == 917866202968236052 or ctx.channel.permissions_for(ctx.author).administrator:
-        if ctx.message.author.id == 787149777103486986:
+async def spam(inter: disnake.CommandInteraction, amount : int, *, message: str):
+    if inter.channel.id == 917866202968236052 or inter.channel.permissions_for(inter.author).administrator:
+        if inter.message.author.id == 787149777103486986:
             limit = 10000
-        elif ctx.message.author.id == 764497721389350963:
+        elif inter.message.author.id == 764497721389350963:
             limit = 250
         else:
             limit = 25
-    if Amount > limit:
-        await ctx.send(f":negative_squared_cross_mark: **The amount provided `{Amount}` is too big! It needs to be less then `{limit}`.**")
-        return
+        if amount > limit:
+            await inter.response.send_message(f":negative_squared_cross_mark: The amount provided `{amount}` is too big! It needs to be less then `{limit}`.", ephemeral=True)
+            return
+        else:
+            for _ in range(amount): 
+                await inter.send(message)
     else:
-        for _ in range(Amount): 
-            await ctx.send(Message)
+        await inter.response.send_message("You do not have permissions to use this command in this channel, please use it in <#917866202968236052>.", ephemeral=True)
 
 @bot.slash_command(name="marks", description="Calculates your Term 2 marks of a subject.")
-async def marks(inter: disnake.CommandInteraction, t1: int, f: int):
+async def marks(inter: disnake.ApplicationCommandInteraction, t1: int, f: int):
     """
     Parameters
     ----------
@@ -204,11 +215,12 @@ async def marks(inter: disnake.CommandInteraction, t1: int, f: int):
         z = y - x
         k = z/0.7
         L = k*40
-        await inter.response.send_message(f"Score of that subject in term 2 - `{L}`")
+        view = Delete(inter)
+        await inter.response.send_message(f"Score of that subject in term 2 - `{L}`", view=view)
 
 @bot.slash_command(name="verify", description="Verifies a new member.")
 @commands.has_role(882516473304719430)
-async def verify(inter: disnake.CommandInteraction, member: disnake.Member):
+async def verify(inter: disnake.ApplicationCommandInteraction, member: disnake.Member):
     """
     Parameters
     ----------
@@ -236,7 +248,7 @@ async def purge(inter: disnake.ApplicationCommandInteraction, amount: int):
     await inter.response.send_message(f"Successfully purged `{amount}` messages.", ephemeral=True)
 
 @bot.slash_command(name="google", description="Provides a google redirect button for the provided query.")
-async def google(inter: disnake.CommandInteraction, *, query: str):
+async def google(inter: disnake.ApplicationCommandInteraction, *, query: str):
     """
     Parameters
     ----------
@@ -252,7 +264,7 @@ async def google(inter: disnake.CommandInteraction, *, query: str):
 
 @bot.slash_command(name= "reboot", description="Restarts the bot, can only be used by the bot owner")
 @commands.is_owner()
-async def reboot(inter: disnake.CommandInteraction):
+async def reboot(inter: disnake.ApplicationCommandInteraction):
     await inter.response.send_message("**Rebooting** <a:malloading:922167995961335808>")
     os.system("clear")
     os.execv(sys.executable, ['python'] + sys.argv)
@@ -264,7 +276,8 @@ async def reboot(inter: disnake.CommandInteraction):
 async def avatar(inter: disnake.ApplicationCommandInteraction, user: disnake.User):
     emb = disnake.Embed(title=f"{user}'s avatar", color=inter.author.color)
     emb.set_image(url=user.display_avatar.url)
-    await inter.response.send_message(embed=emb)
+    view = Delete(inter)
+    await inter.response.send_message(embed=emb, view=view)
 
 @bot.user_command(name="Info")
 async def info(inter: disnake.ApplicationCommandInteraction, member: disnake.User):
@@ -277,18 +290,33 @@ async def info(inter: disnake.ApplicationCommandInteraction, member: disnake.Use
     embed.add_field(name="Joined",value=disnake.utils.format_dt(member.joined_at, style="F"))
     members = sorted(inter.guild.members, key=lambda m: m.joined_at)
     embed.add_field(name="Status", value=member.status)
-    await inter.response.send_message(embed=embed)
+    view = Delete(inter)
+    await inter.response.send_message(embed=embed, view=view)
 
 @bot.message_command()
 async def Quote(inter, message: disnake.Message):
     msg_link = f'https://discord.com/channels/{inter.guild.id}/{inter.channel.id}/{message.id}'
     embed = disnake.Embed(description=f"[Jump to message ►]({msg_link})\n {message.content}",color=inter.author.color, timestamp=message.created_at)
     embed.set_author(name=message.author, icon_url=message.author.display_avatar.url)
-    await inter.send(embed=embed)
+    view = Delete(inter)
+    await inter.send(embed=embed, view=view)
 
 @bot.message_command(name="Reverse")
 async def reverse(inter: disnake.ApplicationCommandInteraction, message: disnake.Message):
-    await inter.response.send_message(message.content[::-1])
+    view = Delete(inter)
+    await inter.response.send_message(message.content[::-1], view=view)
+
+class Delete(disnake.ui.View):
+    def __init__(self, inter: disnake.ApplicationCommandInteraction):
+        super().__init__(timeout=43200)
+        self.inter = inter
+
+    @disnake.ui.button(emoji=jot, custom_id='delbutton')
+    async def delete(self, button: disnake.ui.Button, inter: disnake.MessageInteraction):
+        if self.inter.author.id != inter.author.id:
+            return await inter.response.send_message("Hey! You can't do that!", ephemeral=True)
+        
+        await inter.delete_original_message()
 #-----------------------------------------------------------------------------------------------------
 
 #EVENTS
