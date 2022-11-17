7ad8da9258dd814b02016a5a6a7d11e37c6afe05
Rishit Gupta <83908451+Rishit-Gupta-RG@users.noreply.github.com>
2022-11-17 15:44:33 +0530

    Nice

--- cogs/chemistry.py
+++ cogs/chemistry.py
@@ -28,84 +28,36 @@ class Chemistry(commands.Cog):
         number: The atomic number of element.
         """
         if 0 > number or number > 118 or number == 0:
-            await inter.response.send_message("Number must be greater than 0 or less than 118.")
+            await inter.response.send_message("Atomic number cannot be greater than 118 or less than 0.", ephermal=True)
         else:
             async with aiohttp.ClientSession() as session:
                 request = await session.get(f'https://periodic-table-elements-info.herokuapp.com/element/atomicNumber/{number}')
                 data = await request.json()
-            if data["atomicMass"] == "":
-                data["atomicMass"] = "_ _"
-            if data["atomicRadius"] == "":
-                data["atomicRadius"] = "_ _"
-            if data["boilingPoint"] == "":
-                data["boilingPoint"] = "_ _"
-            if data["bondingType"] == "":
-                data["bondingType"] = "_ _"
-            if data["density"] == "":
-                data["density"] = "_ _"
-            if data["electronAffinity"] == "":
-                data["electronAffinity"] = "_ _"
-            if data["electronegativity"] == "":
-                data["electronegativity"] = "_ _"
-            if data["ionRadius"] == "":
-                data["ionRadius"] = "_ _"
-            if data["ionizationEnergy"] == "":
-                data["ionizationEnergy"] = "_ _"
-            if data["meltingPoint"] == "":
-                data["meltingPoint"] = "_ _"
-            if data["oxidationStates"] == "":
-                data["oxidationStates"] = "_ _"
-            if data["standardState"] == "":
-                data["standardState"] = "_ _"
-            if data["vanDelWaalsRadius"] == "":
-                data["vanDelWaalsRadius"] = "_ _"
-            if data["yearDiscovered"] == "":
-                data["yearDiscovered"] = "_ _"
-            if data['cpkHexColor'] == "":
-                embed = disnake.Embed(title=data["name"])
-                embed.add_field(name="Atomic mass:", value=data["atomicMass"])
-                embed.add_field(name="Atomic number:", value=data["atomicNumber"])
-                embed.add_field(name="Atomic radius:", value=data["atomicRadius"])
-                embed.add_field(name="Boiling Point:", value=data["boilingPoint"])
-                embed.add_field(name="Bonding type:", value=data["bondingType"])
-                embed.add_field(name="CPK color:", value="_ _")
-                embed.add_field(name="Density:", value=data["density"])
-                embed.add_field(name="Electron affinity:", value=data["electronAffinity"])
-                embed.add_field(name="Electronegativity:", value=data["electronegativity"])
-                embed.add_field(name="Electronic configuration:", value=data["electronicConfiguration"])
-                embed.add_field(name="Group block:", value=data["groupBlock"])
-                embed.add_field(name="Ion radius:", value=data["ionRadius"])
-                embed.add_field(name="Ionization energy:", value=data["ionizationEnergy"])
-                embed.add_field(name="Melting point:", value=data["meltingPoint"])
-                embed.add_field(name="Oxidation states:", value=data["oxidationStates"])
-                embed.add_field(name="Standard states:", value=data["standardState"])
-                embed.add_field(name="Symbol:", value=data["symbol"])
-                embed.add_field(name="Van der waals radius:", value=data["vanDelWaalsRadius"])
-                embed.add_field(name="Year discovered:", value=data["yearDiscovered"])
-                await inter.response.send_message(embed=embed)
-            else:
-                co = int(data['cpkHexColor'], 16)
-                embed = disnake.Embed(title=data["name"], color=co)
-                embed.add_field(name="Atomic mass:", value=data["atomicMass"])
-                embed.add_field(name="Atomic number:", value=data["atomicNumber"])
-                embed.add_field(name="Atomic radius:", value=data["atomicRadius"])
-                embed.add_field(name="Boiling Point:", value=data["boilingPoint"])
-                embed.add_field(name="Bonding type:", value=data["bondingType"])
-                embed.add_field(name="CPK color:", value=data["cpkHexColor"])
-                embed.add_field(name="Density:", value=data["density"])
-                embed.add_field(name="Electron affinity:", value=data["electronAffinity"])
-                embed.add_field(name="Electronegativity:", value=data["electronegativity"])
-                embed.add_field(name="Electronic configuration:", value=data["electronicConfiguration"])
-                embed.add_field(name="Group block:", value=data["groupBlock"])
-                embed.add_field(name="Ion radius:", value=data["ionRadius"])
-                embed.add_field(name="Ionization energy:", value=data["ionizationEnergy"])
-                embed.add_field(name="Melting point:", value=data["meltingPoint"])
-                embed.add_field(name="Oxidation states:", value=data["oxidationStates"])
-                embed.add_field(name="Standard states:", value=data["standardState"])
-                embed.add_field(name="Symbol:", value=data["symbol"])
-                embed.add_field(name="Van der waals radius:", value=data["vanDelWaalsRadius"])
-                embed.add_field(name="Year discovered:", value=data["yearDiscovered"])
-                await inter.response.send_message(embed=embed)
+            if data['cpkHexColor'] == "unknown":
+                EmbedColor = None
+            else: 
+                EmbedColor = int(data['cpkHexColor'], 16)
+            embed = disnake.Embed(title=data["name"], color=EmbedColor)
+            embed.add_field(name="Atomic mass:", value=data["atomicMass"])
+            embed.add_field(name="Atomic number:", value=data["atomicNumber"])
+            embed.add_field(name="Atomic radius:", value=data["atomicRadius"])
+            embed.add_field(name="Boiling Point:", value=data["boilingPoint"])
+            embed.add_field(name="Bonding type:", value=data["bondingType"])
+            embed.add_field(name="CPK color:", value=data["cpkHexColor"])
+            embed.add_field(name="Density:", value=data["density"])
+            embed.add_field(name="Electron affinity:", value=data["electronAffinity"])
+            embed.add_field(name="Electronegativity:", value=data["electronegativity"])
+            embed.add_field(name="Electronic configuration:", value=data["electronicConfiguration"])
+            embed.add_field(name="Group block:", value=data["groupBlock"])
+            embed.add_field(name="Ion radius:", value=data["ionRadius"])
+            embed.add_field(name="Ionization energy:", value=data["ionizationEnergy"])
+            embed.add_field(name="Melting point:", value=data["meltingPoint"])
+            embed.add_field(name="Oxidation states:", value=data["oxidationStates"])
+            embed.add_field(name="Standard states:", value=data["standardState"])
+            embed.add_field(name="Symbol:", value=data["symbol"])
+            embed.add_field(name="Van der waals radius:", value=data["vanDelWaalsRadius"])
+            embed.add_field(name="Year discovered:", value=data["yearDiscovered"])
+            await inter.response.send_message(embed=embed)
 
 def setup(bot):
     bot.add_cog(Chemistry(bot))
