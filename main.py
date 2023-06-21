53b87dab942494a9ad3e804c6aa58fc5712a594f
Rishit Gupta <83908451+Rishit-Gupta-RG@users.noreply.github.com>
2023-06-22 00:35:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -227,7 +227,7 @@ async def wolfram(inter: disnake.ApplicationCommandInteraction, question: str):
     ans = next(send.results).text
     await inter.response.send_message(f"> {question}\n{ans}")
           
-openai.api_key = "OPENAI_API_KEY"
+openai.api_key = os.getenv("TOKEN")
 @bot.command(name="gpt", description="[BETA] Sends a query to ChatGPT.")
 async def gpt(ctx, *, query: str):
     messages = [{"role": "user", "content": "the content to ask"}]
